3bb2b658772c0058f85ce09c6331cbf122281607
arzelcm <arzelcanavate@gmail.com>
2024-09-13 14:39:47 +0200

    Add time and logs

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: arcanava <arcanava@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 13:46:47 by arcanava          #+#    #+#             */
-/*   Updated: 2024/09/12 20:41:57 by arcanava         ###   ########.fr       */
+/*   Updated: 2024/09/13 14:38:39 by arcanava         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,19 @@
 #include <sys/time.h>
 #include "ft_atoi.h"
 #include "table.h"
+#include <pthread.h>
+#include <unistd.h>
+
+long	get_time_now(struct timeval start)
+{
+	struct timeval	now;
+
+	(void) start;
+	if (gettimeofday(&now, NULL) != 0)
+		return (-1);
+	return (((now.tv_sec * 1000) + (now.tv_usec / 1000))
+		- ((start.tv_sec * 1000) + (start.tv_usec / 1000)));
+}
 
 int	correct_args(int argc, char **argv)
 {
@@ -40,32 +53,55 @@ void	*philo_live(void *param)
 {
 	t_philo	*philo;
 
-	philo = param;
-	printf("[%i] Philo living(%lu)\neating: %i, thinking: %i, sleeping: %i, color: %s\n\n",
-			philo->index, philo->thread, philo->eating, philo->thinking, philo->sleeping, philo->color);
+	philo = (t_philo *) param;
+	// pthread_mutex_lock(&philo->mutex);
+	// pthread_mutex_lock(&(philo->table->mutex));
+	printf("%s%lu %i has taken a seat\n"DEF_COLOR,
+		philo->color, get_time_now(philo->table->start_time), philo->index);
 	return (NULL);
 }
 
-t_philo	*create_philos(int amount, t_table *table)
+void	*create_philos(int amount, t_table *table)
 {
 	int			i;
+	char		*colors[COLORS_AMOUNT];	
 
-	gettimeofday(&table->startTime, NULL);
+	colors[0] = RED;
+	colors[1] = GREEN;
+	colors[2] = YELLOW;
+	colors[3] = BLUE;
+	colors[4] = PINK;
+	colors[5] = CYAN;
+	table->colors_amount = 4;
+	if (gettimeofday(&table->start_time, NULL) != 0)
+		return (table->philos = NULL, NULL);
+	table->philos_amount = amount;
 	table->philos = malloc(sizeof(t_philo) * amount);
 	if (!table->philos)
-		return(NULL);
+		return (table->philos = NULL, NULL);
 	memset(table->philos, 0, sizeof(t_philo) * amount);
 	i = 0;
 	while (i < amount)
 	{
+		table->philos[i].color = colors[i % COLORS_AMOUNT];
+		table->philos[i].table = table;
+		pthread_mutex_init(&(table->philos + i)->mutex, NULL);
 		table->philos[i].index = i + 1;
-		if (pthread_create(&table->philos[i].thread, NULL, philo_live, table->philos + i))
-			return(NULL);
-		printf("Creating philo\n");
-		pthread_join(table->philos[i].thread, NULL);
+		if (pthread_create(&table->philos[i].thread, NULL,
+				philo_live, table->philos + i))
+			return (table->philos = NULL, NULL);
 		i++;
 	}
 	return (table->philos);
+}
+
+void	join_threads(t_table *table)
+{
+	int	i;
+
+	i = 0;
+	while (i < table->philos_amount)
+		pthread_join(table->philos[i++].thread, NULL);
 }
 
 int	main(int argc, char **argv)
@@ -77,5 +113,7 @@ int	main(int argc, char **argv)
 	create_philos(ft_atoi(argv[1]), &table);
 	if (!table.philos)
 		return (free(table.philos), EXIT_FAILURE);
+	else
+		join_threads(&table);
 	return (free(table.philos), EXIT_SUCCESS);
 }
--- philo/table.h
+++ philo/table.h
@@ -6,16 +6,26 @@
 /*   By: arcanava <arcanava@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 19:45:15 by arcanava          #+#    #+#             */
-/*   Updated: 2024/09/12 20:41:50 by arcanava         ###   ########.fr       */
+/*   Updated: 2024/09/13 14:39:28 by arcanava         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TABLE_H
 # define TABLE_H
+# define COLORS_AMOUNT 6
+# define DEF_COLOR "\033[0m"
+# define RED "\033[1;31m"
+# define GREEN "\033[1;32m"
+# define YELLOW "\033[1;33m"
+# define BLUE "\033[1;34m"
+# define PINK "\033[1;35m"
+# define CYAN "\033[1;36m"
 
 # include <sys/time.h>
 
-struct s_table;
+typedef struct timeval	t_timeval;
+
+struct					s_table;
 
 typedef struct s_philo
 {
@@ -28,11 +38,16 @@ typedef struct s_philo
 	struct s_table	*table;
 	struct s_philo	*prev;
 	struct s_philo	*next;
+	pthread_mutex_t	mutex;
 }	t_philo;
 
 typedef struct s_table
 {
-	t_philo	*philos;
-	struct timeval		startTime;
+	t_philo			*philos;
+	int				philos_amount;
+	pthread_mutex_t	mutex;
+	t_timeval		start_time;
+	char			*colors[COLORS_AMOUNT];
+	int				colors_amount;
 }	t_table;
 #endif

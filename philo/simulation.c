0d66393b66de22b67a9c0dd86bad637a5ca93860
arzelcm <arzelcanavate@gmail.com>
2024-10-03 15:50:40 +0200

    Add minimum meals monitoring

--- philo/simulation.c
+++ philo/simulation.c
@@ -6,7 +6,7 @@
 /*   By: arcanava <arcanava@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 17:57:46 by arcanava          #+#    #+#             */
-/*   Updated: 2024/10/02 14:27:43 by arcanava         ###   ########.fr       */
+/*   Updated: 2024/10/03 15:50:03 by arcanava         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,31 +36,44 @@ void	set_colors(char **colors)
 	colors[5] = RED;
 }
 
-void	*check_simulation_routine(void *param)
+void	monitor_finish(t_table *table, int *i, int *philos_finished)
 {
-	t_table	*table;
-	int		finished;
-	int		i;
+	int	finished;
 
-	table = (t_table *) param;
 	finished = 0;
 	while (!finished)
 	{
-		i = 0;
-		while (i < table->philos_amount && !finished)
+		*i = 0;
+		*philos_finished = 0;
+		while (*i < table->philos_amount && !finished)
 		{
-			pthread_mutex_lock(&table->philos[i].mutex);
-			finished = get_time_now(table->philos[i].last_eat)
+			pthread_mutex_lock(&table->philos[*i].mutex);
+			finished = get_time_now(table->philos[*i].last_eat)
 				>= table->time_death;
-			pthread_mutex_unlock(&table->philos[i].mutex);
-			i++;
+			*philos_finished += table->philos[*i].eaten_times
+				== table->times_eat;
+			pthread_mutex_unlock(&table->philos[*i].mutex);
+			(*i)++;
 		}
+		finished |= table->times_eat > 0
+			&& *philos_finished == table->philos_amount;
 	}
+}
+
+void	*check_simulation_routine(void *param)
+{
+	t_table	*table;
+	int		philos_finished;
+	int		i;
+
+	table = (t_table *) param;
+	monitor_finish(table, &i, &philos_finished);
 	pthread_mutex_lock(&table->finished_mutex);
 	table->finished = 1;
 	pthread_mutex_unlock(&table->finished_mutex);
-	pthread_mutex_lock(&table->log_mutex);
-	print_vital_message(&table->philos[i - 1], "is dead", RED);
-	pthread_mutex_unlock(&table->log_mutex);
+	if (philos_finished != table->philos_amount)
+		print_vital_message(&table->philos[i - 1], "is dead", RED);
+	else
+		printf(GREEN"\nEveryone finished eating. Cheers!\n"RESET_COLOR);
 	return (NULL);
 }
